5468298c2b5b86aaa55864f9f90556831e748c7b
tanvir ahammad joy <tanvirahammadjoy@gmail.com>
2024-03-19 07:44:00 +0800

    register user updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,44 @@
+import { User } from "../models/user.model";
 import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+const generateAccessAndrefreshTokens = async (userid) => {
+  try {
+    const user = await User.findById(userid);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Somthing wrong while generating refresh and access token",
+    );
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
-    // get user details from frontend
-    // validation - not empty
-    // check if user already exists: username, email
-    // check for images, check for avatar
-    // upload them to cloudinary, avatar
-    // create user object - create entry in db
-    // remove password and refresh token field from response
-    // check for user creation
-    // return res
-    const {fullName, email, username, password } = req.body
-    //console.log("email: ", email);
-})
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exists: username, email
+  // check for images, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
+  const { fullName, email, username, password } = req.body;
+  //console.log("email: ", email);
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+});
+
+export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { router } from "express";
 import { upload } from "../middlewares/multer.middleware";
 import { registerUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = router();
 
@@ -17,5 +18,6 @@ router.route("/register").post(
   ]),
   registerUser,
 );
+router.route("/login").post(loginUser)
 
 export default router;

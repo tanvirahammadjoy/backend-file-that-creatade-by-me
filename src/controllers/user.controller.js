f67f4db8e257d3a7936285668c74e60a62ede73a
tanvir ahammad joy <tanvirahammadjoy@gmail.com>
2024-03-21 00:37:22 +0800

    cloudnari controler updets

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,15 +17,18 @@ const registerUser = asyncHandler(async (req, res) => {
   // check for user creation
   // return res
 
+  // Get user details from the request body
   const { fullName, email, username, password } = req.body;
   //console.log("email: ", email);
 
+  // Validate that all required fields are provided
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All fields are required");
   }
 
+  // Check if a user with the same email or username already exists
   const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
@@ -35,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
   //console.log(req.files);
 
+  // Check if an avatar file is provided in the request
   const avatarLocalPath = req.files?.avatar[0]?.path;
   // const coverImageLocalPath = req.files?.coverImage[0]?.path; // or
 
@@ -48,20 +52,23 @@ const registerUser = asyncHandler(async (req, res) => {
     coverImageLocalPath = req.files.coverImage[0].path;
   }
 
+  // Validate that an avatar file is provided
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
   }
 
+  // Upload avatar and cover image files to Cloudinary
   const avatarUploadResponse = await uploadOnCloudinary(avatarLocalPath);
   const coverImageUploadResponse =
     await uploadOnCloudinary(coverImageLocalPath);
 
+  // Validate that the avatar file is successfully uploaded
   if (!avatarUploadResponse) {
     throw new ApiError(500, "Failed to upload avatar file to Cloudinary");
   }
-
   // Use avatarUploadResponse and coverImageUploadResponse to obtain the URLs for avatar and cover image
 
+  // Create a new user object and save it to the database
   const user = await User.create({
     fullName,
     avatar: avatarUploadResponse.url,
@@ -71,14 +78,17 @@ const registerUser = asyncHandler(async (req, res) => {
     username: username.toLowerCase(),
   });
 
+  // Find the newly created user from the database and exclude sensitive fields
   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken",
   );
 
+  // Validate that the user is successfully created
   if (!createdUser) {
     throw new ApiError(500, "Something went wrong while registering the user");
   }
 
+  // Return success response with the created user details
   return res
     .status(201)
     .json(new ApiResponse(200, createdUser, "User registered Successfully"));
